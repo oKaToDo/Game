f94b4871862f660db8bfb2a8628e0a838249eb21
oKaToDo <L0stost@yandex.ru>
2020-12-23 11:49:19 +0300

    alfa v1.0.1 Добавлен тестовый спрайт танка игрока

--- Main_script.py
+++ Main_script.py
@@ -3,16 +3,23 @@ import pygame
 #Классы игровых объектов
 
 
-class Player:
-    def __init__(self, fps):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, fps, screen):
+        pygame.sprite.Sprite.__init__(self)
         #начальное положение игрока
         self.pos_x = 100
         self.pos_y = 100
-        self.speed = 80 #скорость пискелей в секунду
+        self.speed = 90  # скорость пискелей в секунду
+
+        self.image = pygame.image.load('Sprites/Ресурс 1.png')
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
 
         self.pressedKeys = {pygame.K_w: False, pygame.K_s: False,
                             pygame.K_a: False, pygame.K_d: False}
         self.fps = fps
+        self.sprite = pygame.image.load('Sprites/Player.png')
+        self.sprite.set_colorkey((255, 255, 255))
 
     def move(self, key):
         #изменение координат игрока в зависимости от кнопки
@@ -26,6 +33,7 @@ class Player:
                 self.pos_x += self.speed / self.fps
             if key == pygame.K_a:
                 self.pos_x -= self.speed / self.fps
+        self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
 
 
 
@@ -44,27 +52,27 @@ class Wall:
 def Game():
     running = True
     fps = 60
-    size = width, height = 800, 600
-    player = Player(fps)
+    size = width, height = 1280, 1000
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(size)
+    player = Player(fps, screen)
     key_pressed = None
     while running:
-        pygame.display.update()
         screen.fill((0, 0, 0))
-        #пока что вместо спрайта танка просто кружок)
-        pygame.draw.circle(screen, (0, 0, 255), (player.pos_x, player.pos_y), 20)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 key_pressed = event.key
-                player.pressedKeys[event.key] = True
+                value = True
             if event.type == pygame.KEYUP:
-                player.pressedKeys[event.key] = False
+                value = False
                 print(player.pressedKeys)
-        player.move(key_pressed)
-
+        if key_pressed in player.pressedKeys.keys():
+            player.pressedKeys[key_pressed] = value
+            player.move(key_pressed)
+        screen.blit(player.image, player.rect)
+        pygame.display.update()
         clock.tick(fps)
 
 

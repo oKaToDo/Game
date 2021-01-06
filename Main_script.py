1bfefd32e88a24746747db1acaabce4a3169e04b
oKaToDo <L0stost@yandex.ru>
2021-01-06 16:36:04 +0300

    alfa v1.0.3 Исправлен баг со снарядами

--- Main_script.py
+++ Main_script.py
@@ -4,7 +4,7 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, fps):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         #начальное положение игрока
         self.pos_x = 100
@@ -17,50 +17,43 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect(topleft=(self.pos_x, self.pos_y))
 
-        self.pressedKeys = {pygame.K_w: False, pygame.K_s: False,
-                            pygame.K_a: False, pygame.K_d: False, pygame.K_SPACE: False}
+        self.rect = self.image.get_rect()
         self.lastKey = ''
-        self.fps = fps
         self.image.set_colorkey((255, 255, 255))
 
-    def actions(self, key):
+    def update(self):
         #изменение координат игрока в зависимости от кнопки
-        if key != None and self.pressedKeys[key]:
-            if key == pygame.K_w:
-                self.pos_y -= self.speed // self.fps
-                self.rect.y -= self.speed // self.fps
-                self.image = pygame.image.load('Sprites/Ресурс 1.png')
-                self.lastKey = key
-            if key == pygame.K_s:
-                self.pos_y += self.speed // self.fps
-                self.rect.y += self.speed // self.fps
-                self.image = pygame.image.load('Sprites/Ресурс 3.png')
-                self.lastKey = key
-            if key == pygame.K_d:
-                self.pos_x += self.speed // self.fps
-                self.rect.x += self.speed // self.fps
-                self.image = pygame.image.load('Sprites/Ресурс 2.png')
-                self.lastKey = key
-            if key == pygame.K_a:
-                self.pos_x -= self.speed // self.fps
-                self.rect.x -= self.speed // self.fps
-                self.image = pygame.image.load('Sprites/Ресурс 4.png')
-                self.lastKey = key
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]:
+            self.rect.y -= self.speed // 60
+            self.image = pygame.image.load('Sprites/Ресурс 1.png')
+            self.lastKey = 'w'
+        if key[pygame.K_s]:
+            self.rect.y += self.speed // 60
+            self.image = pygame.image.load('Sprites/Ресурс 3.png')
+            self.lastKey = 's'
+        if key[pygame.K_d]:
+            self.rect.x += self.speed // 60
+            self.image = pygame.image.load('Sprites/Ресурс 2.png')
+            self.lastKey = 'd'
+        if key[pygame.K_a]:
+            self.rect.x -= self.speed // 60
+            self.image = pygame.image.load('Sprites/Ресурс 4.png')
+            self.lastKey = 'a'
 
         self.image = pygame.transform.scale(self.image, (60, 60))
-        self.last_posx = self.pos_x
-        self.last_posy = self.pos_y
         print(self.pos_x, self.pos_y, self.rect.x, self.rect.y)
+        print(self.rect)
 
     def shoot(self):
-        if self.lastKey == pygame.K_w:
+        if self.lastKey == 'w':
             bullet = Bullet(self.rect.centerx, self.rect.top, self.lastKey)
-        elif self.lastKey == pygame.K_s:
+        elif self.lastKey == 's':
             bullet = Bullet(self.rect.centerx, self.rect.bottom, self.lastKey)
-        elif self.lastKey == pygame.K_a:
-            bullet = Bullet(self.rect.centerx, self.rect.left, self.lastKey, notSide=False)
-        elif self.lastKey == pygame.K_d:
-            bullet = Bullet(self.rect.centerx, self.rect.right, self.lastKey, notSide=False)
+        elif self.lastKey == 'a':
+            bullet = Bullet(self.rect.centerx - 30, self.rect.y + 30, self.lastKey, notSide=False)
+        elif self.lastKey == 'd':
+            bullet = Bullet(self.rect.centerx + 30, self.rect.y + 30, self.lastKey, notSide=False)
         sprites.add(bullet)
         bullets.add(bullet)
 
@@ -68,8 +61,6 @@ class Player(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, direction, notSide=True):
         pygame.sprite.Sprite.__init__(self)
-        self.pos_x = x
-        self.pox_y = y
         self.dir = direction
 
         if notSide is False:
@@ -82,13 +73,13 @@ class Bullet(pygame.sprite.Sprite):
         self.speed = 10
 
     def update(self):
-        if self.dir == pygame.K_w:
+        if self.dir == 'w':
             self.rect.y -= self.speed
-        elif self.dir == pygame.K_s:
+        elif self.dir == 's':
             self.rect.y += self.speed
-        elif self.dir == pygame.K_a:
+        elif self.dir == 'a':
             self.rect.x -= self.speed
-        elif self.dir == pygame.K_d:
+        elif self.dir == 'd':
             self.rect.x += self.speed
 
 
@@ -112,27 +103,23 @@ if __name__ == '__main__':
     size = width, height = 1280, 1000
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(size)
-    player = Player(fps)
+    player = Player()
     sprites = pygame.sprite.Group()
+    sprites.add(player)
     bullets = pygame.sprite.Group()
+    sprites.add(player)
     key_pressed = None
     while running:
-        screen.fill((0, 0, 0))
+        clock.tick(fps)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                key_pressed = event.key
-                value = True
             if event.type == pygame.KEYUP:
-                value = False
                 if event.key == pygame.K_SPACE:
                     player.shoot()
-        if key_pressed in player.pressedKeys.keys():
-            player.pressedKeys[key_pressed] = value
-            player.actions(key_pressed)
-        screen.blit(player.image, player.rect)
         sprites.update()
+
+        screen.fill((0, 0, 0))
         sprites.draw(screen)
         pygame.display.flip()
-        clock.tick(fps)
+pygame.quit()

61dde2259f1bf0d3ee75c1aa834bd925fc956970
oKaToDo <L0stost@yandex.ru>
2021-01-08 15:53:55 +0300

    alfa v1.0.5 Добавлен тестовый спрайт стены и один пассивный танк противника для тестов.

--- Main_script.py
+++ Main_script.py
@@ -1,24 +1,29 @@
 import pygame
 
-#Классы игровых объектов
+
+# Классы игровых объектов
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        #начальное положение игрока
+        # начальное положение игрока
         self.speed = 150  # скорость пискелей в секунду
 
         self.image = pygame.image.load('Sprites/Ресурс 1.png')
         self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect()
         self.rect.center = (640, 960)
+        self.lastRect_x = self.rect.x
+        self.lastRect_y = self.rect.y
 
         self.lastKey = ''
         self.image.set_colorkey((255, 255, 255))
 
+        self.hp = 10  # хп танка (уничтожение от 2 попаданий, урон снарядом противника - 5 хп)
+
     def update(self):
-        #изменение координат игрока в зависимости от кнопки
+        # изменение координат игрока в зависимости от кнопки
         key = pygame.key.get_pressed()
         sp = self.speed // 60
         if key[pygame.K_w]:
@@ -38,6 +43,8 @@ class Player(pygame.sprite.Sprite):
             self.image = pygame.image.load('Sprites/Ресурс 4.png')
             self.lastKey = 'a'
 
+        self.lastRect_x = self.rect.x
+        self.lastRect_y = self.rect.y
         self.image = pygame.transform.scale(self.image, (60, 60))
         self.check_border()
 
@@ -51,20 +58,29 @@ class Player(pygame.sprite.Sprite):
             bullet = Bullet(self.rect.centerx - 30, self.rect.y + 30, self.lastKey, notSide=False)
         elif self.lastKey == 'd':
             bullet = Bullet(self.rect.centerx + 30, self.rect.y + 30, self.lastKey, notSide=False)
-        sprites.add(bullet)
+        all_sprites.add(bullet)
         bullets.add(bullet)
 
-    def check_border(self):
+    def check_border(self, is_collided=False):
         sp = self.speed // 60
-
-        if self.rect.left + sp < 0:
-            self.rect.left = 0
-        if self.rect.right + sp > 1280:
-            self.rect.right = 1280
-        if self.rect.top + sp < 0:
-            self.rect.top = 0
-        if self.rect.bottom + sp > 1000:
-            self.rect.bottom = 1000
+        if is_collided is False:
+            if self.rect.left + sp < 0:
+                self.rect.left = 0
+            if self.rect.right + sp > 1280:
+                self.rect.right = 1280
+            if self.rect.top + sp < 0:
+                self.rect.top = 0
+            if self.rect.bottom + sp > 1000:
+                self.rect.bottom = 1000
+        else:
+            if self.lastKey == 'w':
+                self.rect.y = self.rect.y + sp
+            elif self.lastKey == 's':
+                self.rect.y = self.rect.y - sp
+            elif self.lastKey == 'a':
+                self.rect.x = self.rect.x + sp
+            elif self.lastKey == 'd':
+                self.rect.x = self.rect.x - sp
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -79,7 +95,7 @@ class Bullet(pygame.sprite.Sprite):
         self.image.fill((255, 255, 0))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.speed = 10
+        self.speed = 20
 
     def update(self):
         if self.dir == 'w':
@@ -91,33 +107,104 @@ class Bullet(pygame.sprite.Sprite):
         elif self.dir == 'd':
             self.rect.x += self.speed
 
+        if self.rect.left < 0 or self.rect.right > 1280 or \
+                self.rect.top < 0 or self.rect.bottom > 1000:
+            self.kill()
 
 
 class Enemy(Player):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 150  # скорость пискелей в секунду
+
+        self.image = pygame.image.load('Sprites/Ресурс 3.png')
+        self.image = pygame.transform.scale(self.image, (60, 60))
+        self.rect = self.image.get_rect()
+        self.rect.center = (640, 40)
+
+        self.lastKey = ''
+        self.image.set_colorkey((255, 255, 255))
+
+        self.hp = 10
+
+    def update(self):
+        pass
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.bottom, 's')
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Bush(pygame.sprite.Sprite):
     pass
 
 
-class Bush:
-    pass
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, start_x, start_y):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('Sprites/brick.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (start_x, start_y)
+        self.image.set_colorkey((255, 255, 255))
+        self.hp = 20
+
+    def update(self):
+        if self.hp == 15:
+            pass
+        elif self.hp == 10:
+            pass
+        elif self.hp == 5:
+            pass
+        elif self.hp == 0:
+            self.kill()
 
 
-class Wall:
-    pass
+def check_collide():
+    global running, player, enemy
+    # проверки на попадания пуль и на столкновения с объектами
+    if pygame.sprite.groupcollide(enemies, bullets, False, True):
+        enemy.hp -= 5
+        if enemy.hp == 0:
+            enemy.kill()
+
+    if pygame.sprite.groupcollide(landscape, bullets, False, True):
+        wall.hp -= 5
+
+    if pygame.sprite.spritecollide(player, bullets, True):
+        player.hp -= 5
+        if player.hp == 0:
+            running = False
+
+    if pygame.sprite.spritecollide(player, landscape, False):
+        player.check_border(is_collided=True)
 
 
 if __name__ == '__main__':
     pygame.init()
     running = True
+    cooldown_player = 0
     fps = 60
     size = width, height = 1280, 1000
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(size)
+
     player = Player()
-    sprites = pygame.sprite.Group()
-    sprites.add(player)
+    enemy = Enemy()
+    all_sprites = pygame.sprite.Group()
+    landscape = pygame.sprite.Group()
     bullets = pygame.sprite.Group()
-    sprites.add(player)
-    key_pressed = None
+    enemies = pygame.sprite.Group()
+    enemies.add(enemy)
+
+    all_sprites.add(player)
+    all_sprites.add(enemy)
+    for _ in range(1):
+        wall = Wall(640, 500)
+        landscape.add(wall)
+        all_sprites.add(wall)
+
     while running:
         clock.tick(fps)
         for event in pygame.event.get():
@@ -126,9 +213,15 @@ if __name__ == '__main__':
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
                     player.shoot()
-        sprites.update()
+        #if not cooldown:
+            #enemy.shoot()
+            #cooldown = 100
+        #else:
+            #cooldown -= 1
+
+        all_sprites.update()
+        check_collide()
 
         screen.fill((0, 0, 0))
-        sprites.draw(screen)
+        all_sprites.draw(screen)
         pygame.display.flip()
-pygame.quit()
